e6dfdb543acc0f9bf19c9e888f3e5e3e3091d944
JakeBw <bowden9692@gmail.com>
2019-08-06 11:04:50 -0400

    Fleet spawning & Bullet collisions

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ from ship import Ship
 import game_functions as gf
 import pygame
 from pygame.sprite import Group
+from alien import Alien
 
 def run_game():
     pygame.init()
@@ -13,11 +14,20 @@ def run_game():
     pygame.display.set_caption(ai_settings.game_caption) # Set game window title
     ship = Ship(screen, ai_settings) #initialize the ship
     bullets = Group() # creating a group, similar to a list i think for the bullets.
+    aliens = Group()
+    alien = Alien(ai_settings, screen)
+    
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+
     
     while(True):
-        gf.check_events(ai_settings, screen, ship, bullets) 
-        bullets.update() 
-        gf.update_screen(ai_settings, screen, ship, bullets)    
+        bullets.update()    
         ship.update()
+        
+        gf.check_events(ai_settings, screen, ship, bullets) 
+        gf.update_bullets(aliens, bullets)
+        
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+        gf.update_aliens(ai_settings, aliens)
               
 run_game()
