2a6a7a46cfb55d005188048548275a1dfa8c8212
JakeBw <bowden9692@gmail.com>
2019-08-06 12:53:49 -0400

    Final v1.0

--- settings.py
+++ settings.py
@@ -13,8 +13,7 @@ class Settings():
         self.bullets_allowed = 100
         
         self.alien_points = 50
-        self.alien_speed_factor = 500
-        self.fleet_drop_speed = 100
+        self.fleet_drop_speed = 10
         
         self.speedup_scale = 1.1
         self.score_scale = 1.5
